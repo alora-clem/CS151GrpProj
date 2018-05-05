aee5d2b689d83fddcb0aca7dd3f22a8d399f36b3
Sidqghoman <36757015+Sidqghoman@users.noreply.github.com>
2018-05-04 23:41:19 -0700

    Update CircleStyle.java

--- CircleStyle.java
+++ CircleStyle.java
@@ -7,34 +7,37 @@ import java.awt.geom.Ellipse2D;
 
 
 /** 
- * This class is the concrete implementation of BoardStyle interface
- * contains the methods pertaining to display for the game and 
- * 
- * author @sidqdeep
-
+ * author @Sidqdeep, @Alora, @Patrick
  * 
  */
 
-public class CircleStyle implements BoardStyle{
+public class CircleStyle implements BoardStyle
+{
 
 	/**
 	 *Creates an elliptical pit
 	 *@return an ellipse with size 120 by 120
 	 */
-	public Shape getPit() {
+	public Shape getPit() 
+	{
+	
 		return new Ellipse2D.Double(0, 0, 120, 120);
 	}
 
 	/**
 	 *Creates an elliptical mancala
-	 *@param p - Player this mancala belongs to
+	 *@param p the mancala belongs to
 	 *@return an ellipse with size 50 by 500
 	 */
-	public Shape getMancala(Player p) {
+	public Shape getMancala(Player p) 
+	{
+		
 		return new Ellipse2D.Double(0, 0, 50, 500);
 
 	}
 
 
 }
+
+
 
